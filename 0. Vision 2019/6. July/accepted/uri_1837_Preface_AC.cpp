5629b5b9da941764795a33d01cf24a6cfc4aa780
Minhazul Hayat Khan <minhaz1217@users.noreply.github.com>
2019-06-30 14:14:06 +0600

    Day 56

---
+++ 0. Vision 2019/6. July/accepted/uri_1837_Preface_AC.cpp
@@ -0,0 +1,37 @@
+/*
+Minhazul Hayat Khan
+minhaz1217.github.io
+Website: www.minhazul.com
+EWU, Bangladesh
+Problem Name:
+Problem Link:
+Date : 28 / June / 2019
+Comment:
+*/
+#include<bits/stdc++.h>
+//#include<iostream>
+using namespace std;
+#define DEBUG 1
+#define check(a) DEBUG==1?(cout << a << endl):null;
+#define cc(a) DEBUG==1?(cout << a << endl):cout<<"";
+#define msg(a,b) DEBUG==1?(cout << a << " : " << b << endl):cout<<"";
+#define msg2(a,b,c) DEBUG==1?(cout << a << " : " << b << " : " << c << endl):cout<<"";
+#define msg3(a,b,c,d) DEBUG==1?(cout << a << " : " << b << " : " << c << " : " << d << endl):cout<<"";
+#define msg4(a,b,c,d,e) DEBUG==1?(cout << a << " : " << b << " : " << c << " : " << d << " : " << e << endl):cout<<"";
+#define _INIT ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+
+int main(){
+    int a,b,r,q;
+    cin >> a >> b;
+    for(int i=0;i<=1000;i++){
+        r = i;
+        q = (a-r)/b;
+        if( (b*q + r) == a ){
+            break;
+        }
+    }
+    cout << q << " " << r << endl;
+
+    return 0;
+}
