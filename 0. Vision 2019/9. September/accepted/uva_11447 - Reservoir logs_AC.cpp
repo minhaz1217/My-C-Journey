478b1e92a906d24ccb14d68f8cf36714279343f1
Minhazul Hayat Khan <minhaz1217@users.noreply.github.com>
2019-09-06 20:58:09 +0600

    Day 85

---
+++ 0. Vision 2019/9. September/accepted/uva_11447 - Reservoir logs_AC.cpp
@@ -0,0 +1,139 @@
+/*
+Minhazul Hayat Khan
+minhaz1217.github.io
+Website: www.minhazul.com
+EWU, Bangladesh
+Problem Name:
+Problem Link: https://vjudge.net/problem/UVA-11447
+Date : 06 / September / 2019
+Comment: easy enough. the polygon is already given, so no need to run the convex hull. had problem with > and >=
+*/
+#include<bits/stdc++.h>
+//#include<iostream>
+using namespace std;
+#define DEBUG 1
+#define check(a) DEBUG==1?(cout << a << endl):null;
+#define cc(a) DEBUG==1?(cout << a << endl):cout<<"";
+#define msg(a,b) DEBUG==1?(cout << a << " : " << b << endl):cout<<"";
+#define msg2(a,b,c) DEBUG==1?(cout << a << " : " << b << " : " << c << endl):cout<<"";
+#define msg3(a,b,c,d) DEBUG==1?(cout << a << " : " << b << " : " << c << " : " << d << endl):cout<<"";
+#define msg4(a,b,c,d,e) DEBUG==1?(cout << a << " : " << b << " : " << c << " : " << d << " : " << e << endl):cout<<"";
+#define _INIT ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+
+
+#define EPS 1e-9
+struct Point{
+    double x, y;
+    Point(){
+        x = y = 0.0;
+    }
+    Point(double _x, double _y):x(_x), y(_y) {}
+    bool operator == (Point other) const{
+        return (fabs(x - other.x) < EPS && (fabs(y - other.y) < EPS));
+    }
+    bool operator < (Point other) const{
+        if (fabs(x - other.x) > EPS)
+            return x < other.x;
+        return y < other.y;
+    }
+};
+double dist(Point p1, Point p2){
+    return hypot(p1.x - p2.x, p1.y - p2.y);
+}
+
+bool cmp(Point a,Point b) {
+    return a.x < b.x || (a.x == b.x && a.y < b.y);
+}
+
+bool cw(Point a, Point b, Point c) {
+    return a.x*(b.y-c.y)+b.x*(c.y-a.y)+c.x*(a.y-b.y) < 0;
+}
+
+bool ccw(Point a, Point b, Point c) {
+    return a.x*(b.y-c.y)+b.x*(c.y-a.y)+c.x*(a.y-b.y) > 0;
+}
+
+void convexHullCo(vector<Point>& a) {
+    /// generates a CLOCKWISE polygon.
+    // gives right answer for collinear points.
+    if (a.size() == 1)
+        return;
+
+    sort(a.begin(), a.end(), &cmp);
+    Point p1 = a[0], p2 = a.back();
+    vector<Point> up, down;
+    up.push_back(p1);
+    down.push_back(p1);
+    for (int i = 1; i < (int)a.size(); i++) {
+        if (i == a.size() - 1 || cw(p1, a[i], p2)) {
+            while (up.size() >= 2 && !cw(up[up.size()-2], up[up.size()-1], a[i]))
+                up.pop_back();
+            up.push_back(a[i]);
+        }
+        if (i == a.size() - 1 || ccw(p1, a[i], p2)) {
+            while(down.size() >= 2 && !ccw(down[down.size()-2], down[down.size()-1], a[i]))
+                down.pop_back();
+            down.push_back(a[i]);
+        }
+    }
+
+    a.clear();
+    for (int i = 0; i < (int)up.size(); i++)
+        a.push_back(up[i]);
+    for (int i = down.size() - 2; i > 0; i--)
+        a.push_back(down[i]);
+    reverse(a.begin(),a.end());
+}
+
+double polygonArea(const vector<Point> &P){
+    double result = 0.0, x1, y1, x2, y2;
+    for (int i = 0; i < (int)P.size()-1; i++){
+        x1 = P[i].x;
+        x2 = P[i+1].x;
+        y1 = P[i].y;
+        y2 = P[i+1].y;
+        result += (x1 * y2 - x2 * y1);
+    }
+    return fabs(result) / 2.0;
+}
+
+int main(){
+    double tc,n,x,y,w,iw,sw,fw,area,shortageFlag,excessFlag =0, initialWater = 0 ;
+    cin >> tc;
+    while(tc--){
+        vector<Point>poly;
+        cin >> n;
+        for(int i=0;i<n;i++){
+            cin >> x>> y;
+            poly.push_back(Point(x,y));
+        }
+        //poly.push_back(poly[0]);
+        cin >> w;
+        //convexHullCo(poly);
+        poly.push_back(poly[0]);
+        area = polygonArea(poly);
+        shortageFlag = 0;
+        excessFlag = 0;
+        area *= w;
+        cin >> iw >> sw >> fw;
+        initialWater = area*iw/100.0;
+        //msg3(area, initialWater, sw,fw)
+        if(sw > initialWater){
+            shortageFlag = 1;
+        }
+        initialWater = max(0.0, initialWater - sw);
+        if(initialWater + fw > area){
+            excessFlag = 1;
+        }
+        initialWater = min(area, initialWater+fw);
+        if(shortageFlag){
+            cout << "Lack of water. ";
+        }
+        if(excessFlag){
+            cout << "Excess of water. ";
+        }
+        printf("Final percentage: %d%\n", (int)(100.0*initialWater/ area));
+    }
+    return 0;
+}
