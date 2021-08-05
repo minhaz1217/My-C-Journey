5c7e9af9f2424f89e0d2b1cc640d79656ea4d959
mh.khan <minhaz1217@gmail.com>
2021-08-06 01:34:24 +0600

    leetcode daily solved, day 39

---
+++ OJ_LEETCODE/Accepted/3870 - Stone Game_AC.cpp
@@ -0,0 +1,32 @@
+/*
+Minhazul Hayat Khan
+Website: www.minhazul.com
+EWU, Bangladesh
+Problem Name:
+Problem Link: https://leetcode.com/explore/challenge/card/august-leetcoding-challenge-2021/613/week-1-august-1st-august-7th/3870/
+Date : 05 / August / 2021
+Comment: absolutely garbage solution.
+*/
+#include<bits/stdc++.h>
+//#include<iostream>
+using namespace std;
+#define DEBUG 1
+#define check(a) DEBUG==1?(cout << a << endl):null;
+#define cc(a) DEBUG==1?(cout << a << endl):cout<<"";
+#define msg(a,b) DEBUG==1?(cout << a << " : " << b << endl):cout<<"";
+#define msg2(a,b,c) DEBUG==1?(cout << a << " : " << b << " : " << c << endl):cout<<"";
+#define msg3(a,b,c,d) DEBUG==1?(cout << a << " : " << b << " : " << c << " : " << d << endl):cout<<"";
+#define msg4(a,b,c,d,e) DEBUG==1?(cout << a << " : " << b << " : " << c << " : " << d << " : " << e << endl):cout<<"";
+#define _INIT ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        return true;
+    }
+};
+
+int main(){
+
+    return 0;
+}
